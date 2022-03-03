e6c72ffd9ce5b95139d35f530144573f272a8c91
Cameron Li <cameronl14320@gmail.com>
2022-03-04 11:13:35 +1300

    responsive design

--- portal-client/pages/landing.tsx
+++ portal-client/pages/landing.tsx
@@ -3,7 +3,9 @@ import style from './landing.module.scss';
 function Landing(props: any) {
     return (
         <div className={ style.container }>
-            Landing page
+            <div className='l-container'>
+
+            </div>
         </div>
     )
 }
