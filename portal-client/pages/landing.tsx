b0c285c5eed32a4bc67b3e6dc620f9caca625d37
Cameron Li <cameronl14320@gmail.com>
2022-03-04 15:31:09 +1300

    profileCard

--- portal-client/pages/landing.tsx
+++ portal-client/pages/landing.tsx
@@ -1,9 +1,9 @@
 import style from './landing.module.scss';
 import Card from '../components/card/card';
 import colors from '../styles/styles.module.scss';
+import Image from 'next/image';
 
 const landingCardStyle = {
-    backgroundColor: colors.starlightBase,
     width: '100%'
 };
 
@@ -15,13 +15,17 @@ function Landing(props: any) {
                     <Card style={ landingCardStyle }>
                         <div className={ style.profileCard }>
                             <div className={ style.profileCardPrimary}>
-                                
+                                <div className={ style.profileCardInfo }>
+                                    <Image src='http://placekitten.com/300/300' width='200px' height='300px' layout="responsive"></Image>
+                                </div>
                             </div>
                             <div className={ style.profileCardSecondary }>
                                 <div className={ style.profileCardInfo }>
-                                    <div className='subtitle'>Introduction</div>
+                                    <div className={ style.subtitle }>
+                                        <div className='subtitle'>Introduction</div>
+                                    </div>
                                     <div>
-                                        Full stack developer with experience in Angular
+                                        Full stack developer with experience in Angular, React, Java, and Sass.
                                     </div>
                                 </div>
                             </div>
