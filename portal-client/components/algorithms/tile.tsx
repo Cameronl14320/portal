0493f7a8e35296d95969b2f4cbe44e67f0feb82b
Cameron Li <cameronl14320@gmail.com>
2022-04-11 16:19:22 +1200

    semi display

--- portal-client/components/algorithms/tile.tsx
+++ portal-client/components/algorithms/tile.tsx
@@ -1,10 +1,17 @@
-import React, { Dispatch, SetStateAction } from "react";
-import { boardState, pair, tileState } from "./algorithms";
+import React from "react";
+import { pair, tileState } from "./algorithms";
+import style from './tile.module.scss';
+
+const tileColors = [
+    'white', 'green', 'red', 'blue'
+]
 
 export default function Tile(props: {position: pair<number>, state: tileState, handleClick: (position: pair<number>) => void}) {
     return (
-        <div onClick={() => {console.log('clicked'); props.handleClick(props.position)}}>
-            |{props.state}|
+        <div
+            className={style.container}
+            onClick={() => {console.log('clicked'); props.handleClick(props.position)}}
+            style={{ backgroundColor: tileColors[props.state]}}>
         </div>
     )
 }
